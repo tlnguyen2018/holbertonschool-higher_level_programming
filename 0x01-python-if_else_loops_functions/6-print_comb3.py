9348c7433fb8390d1846fec9670d9c4791f871bc
Thang Louis Nguyen <42980824+tlnguyen2018@users.noreply.github.com>
2018-12-04 14:35:52 -0800

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for first in range(10):
     for second in range(10):
         if second > first:
-            print(first, end='')
+            print("{}".format(first), end='')
             if first != 8 or second != 9:
-                print(second, end=', ')
+                print("{}".format(second), end=', ')
             else:
-                print(second)
+                print("{}".format(second))
