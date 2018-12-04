a9927127a162330d614e540c3a18ace72a39f984
Thang Louis Nguyen <42980824+tlnguyen2018@users.noreply.github.com>
2018-12-04 15:53:31 -0800

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 def fizzbuzz():
     i = 1
     while (i <= 100):
-        if (i % 3 == 0):
-            print("Fizz", end=" ")
+        if (i % 15 == 0):
+            print("FizzBuzz", end=" ")
         elif (i % 5 == 0):
             print("Buzz", end=" ")
-        elif (i % 15 == 0):
-            print("FizzBuzz", end=" ")
+        elif (i % 3 == 0):
+            print("Fizz", end=" ")
         else:
             print(i, end=" ")
         i = i + 1
