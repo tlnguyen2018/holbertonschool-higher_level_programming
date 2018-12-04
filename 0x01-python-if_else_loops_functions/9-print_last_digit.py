187e039758d47f15fdfc9775fce9d0471984b8c8
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-12-04 23:19:34 +0000

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        i = ((number * -1) % 10)
+        print("{}".format(i), end="")
+    else:
+        i = (number % 10)
+        print("{}".format(i), end="")
