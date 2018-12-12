beb2118416a2227b07ff540a75f4271a1511c735
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-12-12 05:26:31 +0000

    max value

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if type(my_list) is not list or not my_list:
+        return None
+    max_int = my_list[0]
+    for i in my_list:
+        if i > max_int:
+            max_int = i
+    return max_int
