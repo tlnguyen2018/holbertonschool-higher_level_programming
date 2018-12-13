8d8f41f9fd459f41da174eaa71b52dc472119d42
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-12-13 23:51:55 +0000

    best score best name

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
+    if a_dictionary == {} or a_dictionary is None:
         return (None)
     else:
-        max_sc = max(a_dictionary.keys(), key=(lambda k: a_dictionary[k]))
+        for (k, v) in a_dictionary.items():
+            if v == max(a_dictionary.values()):
+                return (k)
