dc5f08e50cbb93779144ee0f2174a90dc6ac0071
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-12-15 00:35:03 +0000

    delete value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value not in a_dictionary or a_dictionary == {}:
+    if value not in a_dictionary.values() or a_dictionary == {}:
         return (a_dictionary)
-    new_dic = [x for x in a_dictionary if a_dictionary[x] == value]
-    for x in new_dic:
-        del new_dic(x)
-    return (new_dic)
+    new_dict = [x for x in a_dictionary if a_dictionary[x] == value]
+    for x in new_dict:
+        del(new_dict[x])
+    return (new_dict)
