446def6991ee4b0599fbc808cfaf8c78ddc62a84
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-04 01:02:30 +0000

    Square class size is private instance attrinute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Square class: create a Square class with private attributes "size"
+"""
+
+
+class Square:
+    """
+    Initializa private instance attribute "size"
+    """
+    def __init__(self, size):
+        """
+        Keep size as private instance attribute
+        """
+        self.___size = size
