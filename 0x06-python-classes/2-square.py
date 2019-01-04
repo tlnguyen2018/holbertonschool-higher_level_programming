8547ea483453eda213095c2aec9f3735cfb8495c
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-04 01:42:08 +0000

    Square with size, with condition

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,16 +9,15 @@ class Square:
     Initialize the class
     """
 
-
-def __init__(self, size=0):
-    """
-    if size value is not integer, raise typeerror
-    """
-if type(size) != int:
-    raise TypeError("size must be an integer")
-"""
-If size less than 0, value error
-"""
-if size < 0:
-    raise ValueError("size must be >= 0")
-self.__size = size
+    def __init__(self, size=0):
+        """
+        if size value is not integer, raise typeerror
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+            """
+            If size less than 0, value error
+            """
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
