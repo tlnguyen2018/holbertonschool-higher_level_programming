7f385fcb468afe81ee892a48881f95b491bc3341
Thang Louis Nguyen <42980824+tlnguyen2018@users.noreply.github.com>
2019-01-03 17:19:32 -0800

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
 Square class with private instance attribute
-=======
-Square class: create a Square class with private attributes size
->>>>>>> 1ed118c578ba3fc531e626442f0a808d7f452cc7
 """
 
 
 class Square:
     """
-<<<<<<< HEAD
     Initialize the class
-=======
-    Initialize private instance attribute "size
->>>>>>> 1ed118c578ba3fc531e626442f0a808d7f452cc7
     """
     def __init__(self, size):
         self.__size = size
