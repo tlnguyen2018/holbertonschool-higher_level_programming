5d9801c81ed256ce53a5b7e93ed9694704e92460
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-04 00:11:15 +0000

    create empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Square class: create a Square class with no attributes or method
+"""
+
+
+class Square:
+    """
+    Use pass since this is an empty class
+    """
+    pass
