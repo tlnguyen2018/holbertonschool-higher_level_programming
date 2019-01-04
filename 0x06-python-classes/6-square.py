82d5c261db252a23b7d68029bf70b94c8ada8ec5
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-04 06:31:33 +0000

    Fixed systax

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -66,9 +66,19 @@ class Square:
         if self.size == 0:
             print()
         else:
+            """
             a, b = self.position
             for line in range(b):
                 print()
             for line in range(self.size):
                 print(' ' * a, end='')
                 print('#' * self.size)
+            """
+            for p in range(0, self.__position[1]):
+                print("")
+            for i in range(0, self.__size):
+                for p in range(0, self.__position[0]):
+                    print(" ", end="")
+                for i in range(0, self.__size):
+                    print("#", end="")
+            print("")
