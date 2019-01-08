887e7251309298b81d36de83f53c6447e244ed68
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-08 06:01:00 +0000

    addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,22 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+
+
+"""
+Addition python program for a and b
+Using doctest
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Condition: a and b must be integers or floats,
+    otherwise raise a TypeError exception with the message
+    a must be an integer or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
+    """
+    if not (isinstance(a, float) or isinstance(a, int)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, float) or isinstance(b, int)):
+        raise TypeError("b must be an integer")
+    return int(a + b)
