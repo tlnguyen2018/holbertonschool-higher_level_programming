4340006b36e706442b6fee551bba77a5df218502
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-16 23:09:18 +0000

    add 2 public instance, condition for class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Previous project, plus
+"""
+
+
+class BaseGeometry:
+    """
+    Public Instance
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    """
+    Public instance method validator
+    """
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
