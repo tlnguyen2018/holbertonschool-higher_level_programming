e46cb86f321dbcc4967faed341e90fe9a9bf9381
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-16 20:39:13 +0000

    compare type ojects and classs

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Return True if obj same type with a_class
+False for fail
+"""
+
+
+def is_same_class(obj, a_class):
+    if type(obj) is a_class:
+        return (True)
+    else:
+        return (False)
