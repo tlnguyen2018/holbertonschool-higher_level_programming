d25185d5e534ffc727eb96d512e4eeeb97de5617
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-17 00:35:21 +0000

    Change isinstance to type

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,7 +14,7 @@ class BaseGeometry:
     Public instance method validator
     """
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
