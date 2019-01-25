bdf17bdaab8877a046b2aed1c2f8f82c2647dbf9
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-25 23:51:29 +0000

    class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+"""
+Unittest for Rectangle
+"""
+
+from models.base import Base
+from models.rectangle import Rectangle
+import unittest
+
+
+class TestRectangle(unittest.TestCase):
+    def test_no_id(self):
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 1)
+        r4 = Rectangle(1, 1, 0, 0)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r3.id, 3)
+        self.assertEqual(r4.id, 4)
+
+    def text_with_id_and_mix(self):
+        r5 = Rectangle(10, 2, 1, 1, 100)
+        r6 = Rectangle(2, 10, 1, 1, -100)
+        r7 = Rectangle(10, 2, 1, 1)
+        r8 = Rectangle(10, 3, 1, 1)
+        r9 = Rectangle(10, 2, 1, 1, 3.14)
+        r10 = Rectangle(10, 2, 1, 1, "Hello")
+        self.assertEqual(r5.id, 100)
+        self.assertEqual(r6.id, -100)
+        self.assertEqual(r7.id, r4.id + 1)
+        self.assertEqual(r8.id, r7.id + 1)
+        self.assertEqual(r9.id, 3.14)
+        self.assertEqual(r10.id, "Hello")
