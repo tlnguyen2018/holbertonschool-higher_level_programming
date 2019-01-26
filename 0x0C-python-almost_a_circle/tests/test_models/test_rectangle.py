fd307b134fb298e1b9e0ef19c353f0b40ae5998c
tlnguyen2018 <tlnguyen3839@gmail.com>
2019-01-26 01:47:36 +0000

    Update

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,7 +9,7 @@ import unittest
 
 
 class TestRectangle(unittest.TestCase):
-    def test_no_id(self):
+    def test_no_id_mix_case(self):
         r1 = Rectangle(10, 2)
         r2 = Rectangle(2, 10)
         r3 = Rectangle(10, 2, 0, 1)
@@ -18,8 +18,6 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.id, 2)
         self.assertEqual(r3.id, 3)
         self.assertEqual(r4.id, 4)
-
-    def text_with_id_and_mix(self):
         r5 = Rectangle(10, 2, 1, 1, 100)
         r6 = Rectangle(2, 10, 1, 1, -100)
         r7 = Rectangle(10, 2, 1, 1)
@@ -29,6 +27,28 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r5.id, 100)
         self.assertEqual(r6.id, -100)
         self.assertEqual(r7.id, r4.id + 1)
-        self.assertEqual(r8.id, r7.id + 1)
+        self.assertEqual(r8.id, 6)
         self.assertEqual(r9.id, 3.14)
         self.assertEqual(r10.id, "Hello")
+
+    def test_validator_and_edge_case(self):
+        with self.assertRaises(TypeError):
+            Rectangle()
+        with self.assertRaises(TypeError):
+            Rectangle(0)
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "Peter", 3)
+        with self.assertRaises(TypeError):
+            Rectangle("Hello", 1, 3, 10)
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 0, 0, 1)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 3, 5, -7)
+        with self.assertRaises(TypeError):
+            Rectangle(3.14, 5, 10, 15)
+        with self.assertRaises(ValueError):
+            Rectangle(-5, -10)
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "Peter")
+        with self.assertRaises(TypeError):
+            Rectangle((), 1, 2, 3, 4)
